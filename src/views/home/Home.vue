e402243e1cb0d3f735ff8aa4bb94dc1da4ae0eb0
lengxiansheng <945906283@qq.com>
2020-10-20 14:58:24 +0800

    navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,34 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar';
+  // import HomeSwiper from './childComps/HomeSwiper';
 export default {
-  name:"Home"
+  name:"Home",
+  components:{
+    NavBar,
+    // HomeSwiper
+  }
 }
 </script>
-
 <style scoped>
+  #home {
+  padding-top: 44px;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
 
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
 </style>
